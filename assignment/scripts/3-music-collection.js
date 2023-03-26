277b6c4c7526166b705d4827e4cbb0ca308637eb
Vang Vang <vang2code@gmail.com>
2023-03-26 14:36:31 -0500

    solved the for loop

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -6,7 +6,7 @@ function addToCollection (title, artist, yearPulbished) {
     //console.log(test); //testing 
     item = {a:title, b:artist, c:yearPulbished};
     collection.push(item);
-    console.log(collection);
+    //console.log(collection);
     return item;
 
 }
@@ -18,13 +18,15 @@ addToCollection("3Lets go", "3celine", "32012");
 addToCollection("4Lets go", "4celine", "42012");
 addToCollection("5Lets go", "5celine", "52012");
 
-console.log(collection);
+//console.log(collection);
 
 function showCollection(hard){
     for (let i = 0; i < hard.length; i++) {
-        console.log(hard[i]);
-        
+        //console.log(collection[i])
+        //console.log(hard[i].title);
+        console.log(hard[i].a + " by " + hard[i].b + ", published in " + hard[i].c);
     }
-
 }
+
 showCollection(collection);
+

a2065ce751ab68b48ac78fe275b90c5206ccf97c
Vang Vang <vang2code@gmail.com>
2023-03-26 13:10:54 -0500

    finish first part assignment.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection (title, artist, yearPulbished) {
+    //console.log(test); //testing 
+    item = {a:title, b:artist, c:yearPulbished};
+    collection.push(item);
+    return collection;
+}
+
+addToCollection("Lets go", "celine", "2012");
+addToCollection("1Lets go", "1celine", "12012");
+addToCollection("2Lets go", "2celine", "22012");
+addToCollection("3Lets go", "3celine", "32012");
+addToCollection("4Lets go", "4celine", "42012");
+addToCollection("5Lets go", "5celine", "52012");
+
+console.log(collection);
